4e61e275b1e2867457bbb11a2e8b8cc6f9b24ae4
Xhepton <90504917+Xhepton@users.noreply.github.com>
2021-12-02 14:21:32 +0100

    Update ch18Drill2.cpp

--- Drill_Chapter18/ch18Drill2.cpp
+++ Drill_Chapter18/ch18Drill2.cpp
@@ -1,44 +1,42 @@
 #include "std_lib_facilities.h"
-
 vector<int> gv(10);
-
 int factorial(int n) {
-	if (n == 1) 
-		return 1;
-	else
-		return n*factorial(n-1);
+    if (n == 1)
+        return 1;
+    else
+        return n*factorial(n-1);
 }
-
 void f(vector<int> a) {
-	
-	vector<int> lv(a.size());
-	for (int i = 0; i<10; i++) {
-		lv[i] = gv[i];
-	}
-	for (int i = 0; i<10; i++) {
-		cout << lv[i] << " ";
-	}
-	vector<int> lv2;
-	lv2 = a;
-	for (int i = 0; i<10; i++) {
-		
-	}
+    
+    vector<int> lv;
+    for (int i = 0; i<10; i++) {
+        lv.push_back(gv[i]);
+    }
+    cout << "gv értékei: ";
+    for (int i = 0; i<10; i++) {
+        cout << lv[i] << " ";
+    }
+    cout << endl;
+    vector<int> lv2(10);
+    lv2 = a;
+    cout << "argumentum értékei: ";
+    for (int i = 0; i<10; i++) {
+        cout << lv2[i] << " ";
+    }
+    cout << endl;
 }
-
 int main() {
-	
-	for (int i = 0; i<10; i++) {
-		gv[i] = pow(2,i);
-	}
-	
-	f(gv);
-	
-	vector<int> vv(10);
-	for (int i = 0; i<10; i++) {
-		vv[i] = factorial(i);
-	}
-	
-	f(vv);
-	
-	return 0;
+    
+    for (int i = 0; i<10; i++) {
+        gv[i] = pow(2,i);
+    }
+    f(gv);
+    vector<int> vv(0);
+    for (int i = 0; i<10; i++) {
+        vv.push_back(factorial(i+1));
+    }
+    
+    f(vv);
+    
+    return 0;
 }
