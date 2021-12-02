c32e9fd9abda836a935aa7eaa2f66ded4bc48aa6
Xhepton <90504917+Xhepton@users.noreply.github.com>
2021-12-02 15:23:21 +0100

    Update ch18Drill.cpp

--- Drill_Chapter18/ch18Drill.cpp
+++ Drill_Chapter18/ch18Drill.cpp
@@ -1,49 +1,51 @@
 #include "std_lib_facilities.h"
-
 int* ga = new int[10];
-
 int factorial(int n) {
-	if (n == 1) 
-		return 1;
-	else
-		return n*factorial(n-1);
+    if (n == 1)
+        return 1;
+    else
+        return n*factorial(n-1);
 }
-
 void f(int* a, int n) {
-	int* la = new int[10];
-	la = ga;
-	for (int i = 0; i<10; i++) {
-		cout << la[i] << " ";
-	}
-	cout << endl;
-	
-	int* p = new int[n];
-	
-	p = a;
-	
-	for (int i = 0; i<n; i++) {
-		cout << p[i] << " ";
-	}
-	cout << endl;
-	
-	delete[] p;
+    int* la = new int[10];
+    for (int i = 0; i<10; i++) {
+        la[i] = ga[i];
+    }
+    cout << "la values: ";
+    for (int i = 0; i<10; i++) {
+        cout << la[i] << " ";
+    }
+    delete[] la;
+    cout << endl;
+    
+    int* p = new int[n];
+    for (int i = 0; i<n; i++) {
+   &nbs p;    p[i] = a[i];
+    }
+    cout << "p values: ";
+    for (int i = 0; i<n; i++) {
+        cout << p[i] << " ";
+    }
+    cout << endl;
+    
+    delete[] p;
 }
-
 int main() {
-	
-	for (int i = 0; i<10; i++) {
-		ga[i] = pow(2, i);
-	}
-	
-	f(ga, 10);
-	
-	int* aa = new int[10];
-	
-	for (int i = 1; i<=10; i++) {
-		aa[i] = factorial(i);
-	}
-	
-	f(aa, 10);
-	
-	return 0;
+    
+    for (int i = 0; i<10; i++) {
+        ga[i] = pow(2, i);
+    }
+    
+    f(ga, 10);
+    delete[] ga;
+    
+    int* aa = new int[10];
+    
+    for (int i = 1; i<=10; i++) {
+        aa[i] = factorial(i);
+    }
+    
+    f(aa, 10);
+    delete[] aa;
+    return 0;
 }
